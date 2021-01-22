67cdfdd246bfd9e5ab838f6c10b26e16f7828330
Glitch (by-pikachu) <none>
2021-01-22 15:54:27 +0000

    💝🐑 Checkpoint ./server.js:25540286/30 ./commands/fun/simprate.js:25540286/1189 ./commands/fun/simpsfor.js:25540286/120

---
+++ commands/fun/simpsfor.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "simprate",
+  aliases: [],
+  description: "Show How Much % Member Are A Simp",
+  usage: "Simprate <Mention Member>",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = Math.floor(Math.random() * 101);
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`Simp Machine`)
+      .setDescription(`${Member.user.username} ${Result}% Simp`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
