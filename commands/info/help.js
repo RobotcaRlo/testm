26b02c4a7b7385144d08263ec74eb09d112cdaaa
Glitch (viridian-striped-porkpie) <none>
2021-02-01 14:42:09 +0000

    🏋🏑 Checkpoint ./commands/info/help.js:27810717/10

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,7 +15,7 @@ module.exports = {
     .setColor(Color)
     .setTitle(`${client.user.username} Commands!`)
     .setDescription(`Use ${Prefix}Help <Command Name> For More Command Information!` + 
-    "\n\n**Fun**\n`Coinflip, Howgay, Meme, Rate, Dicksize, Ascii, Hack, Randomnumber, 8ball, Simprate`" + "\n\n" + "**Moderation**\n`Clear, Mute, Unmute, Kick, Ban, Unban, Warn, Warnings`" + "\n\n"+
+    "\n\n**Fun**\n`Coinflip, Howgay, Meme, Rate, pp, Ascii, Hack, Randomnumber, 8ball, Simprate`" + "\n\n" + "**Moderation**\n`Clear, Mute, Unmute, Kick, Ban, Unban, Warn, Warnings`" + "\n\n"+
     "**Information**\n`Help, Invite, Weather, Userinfo, Serverinfo, Ping, Avatar`")
     .setFooter(`Requested By ${message.author.username}`)
     .setTimestamp();
