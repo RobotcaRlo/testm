39379e2f8501f35b5349f45cb11918eb24392a4b
Glitch (viridian-striped-porkpie) <none>
2021-02-01 14:21:11 +0000

    Rewound to commit b59d64a7b3086af790d7892e587386e2bb3b2051 + 0 edits

---
+++ commands/moderation/slowmode.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "slowmode",
+  aliases: [],
+  description: "put slowmode on a channel",
+  usage: "Slowmode <number> | <Reason>",
+  }  
+
+module.exports.run = async (bot, message) => {
+ const messageArray = message.content.split(' ');
+ const args = messageArray.slice(1);
+
+message.channel.setRateLimitPerUser(args[0]);
+message.channel.send('slowmode is now on ');
+  
+}
