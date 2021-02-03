8a75ee115b9329e2ea4667f999917c0c14a47349
Glitch (discord-bot-pika) <none>
2021-02-03 14:04:13 +0000

    ✋🌍 Checkpoint ./commands/moderation/mute.js:27810717/43

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -25,7 +25,7 @@ module.exports = {
 
     if (!Role)
       return message.channel.send(
-        `Please Create Mute Role | Role Name : Muted`
+        `Please Create The Role Muted`
       );
 
     if (Member.roles.cache.has(Role)) {
